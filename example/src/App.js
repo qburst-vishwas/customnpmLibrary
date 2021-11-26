74864dba8ac300d5e18950b2db7b51e85be17cd1
vishwasQBurst <vishwas@qburst.com>
2021-11-26 12:26:30 +0530

    first

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
-import { ExampleComponent } from 'custom-lib-vis'
+import {ExampleComponent} from 'custom-lib-vis'
 import 'custom-lib-vis/dist/index.css'
 
+
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+        return (
+        <div style={{padding:"20em"}}>
+          <ExampleComponent>Login page</ExampleComponent>
+        </div>
+       )
+  
 }
 
 export default App
